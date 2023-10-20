0de09dd3bd2f5db8a7736809da7895e74b1f6978
SoujiGitCode <dev.reinaldo.cardenas@gmail.com>
2023-10-20 11:50:06 +0200

    validation folder

---
+++ src/validations/index.tsx
@@ -0,0 +1,8 @@
+
+import * as Yup from 'yup';
+
+export const validationSet1 = {
+    field1: Yup.string().required('Campo 1 es requerido'),
+    field2: Yup.number().required('Campo 2 es requerido'),
+    // Otras validaciones para este conjunto...
+};
--- src/views/Register/index.tsx
+++ src/views/Register/index.tsx
@@ -27,7 +27,7 @@ import useAlert from "@/hooks/useAlert";
 import useAuthStore from "@/hooks/useAuthStore";
 import { PATH } from '@/routes/constants';
 import { ConfirmationModal } from '@/components';
-
+import { registerValidation } from '@/validations/registerValidation';
 
 
 const Register = () => {
@@ -96,88 +96,13 @@ const Register = () => {
       password: "",
       repeatPassword: "",
     },
-    validationSchema: Yup.object({
-      docNumber: Yup.string()
-        .required("Número de documento requerido")
-        .matches(/^[0-9]+$/, "solo debe contener números")
-        .max(20, "máximo 20 caracteres"),
-
-      firstName: Yup.string()
-        .matches(/^[A-Za-z]+$/, "El Primer Nombre solo debe contener letras")
-        .required("Primer Nombre requerido")
-        .max(20, "máximo 20 caracteres"),
-
-      middleName: Yup.string()
-        .matches(/^[A-Za-z]+$/, "El Segundo Nombre solo debe contener letras")
-        .max(20, "máximo 20 caracteres"),
-
-      lastName: Yup.string()
-        .matches(/^[A-Za-z]+$/, "El Apellido solo debe contener letras")
-        .required("Apellido requerido")
-        .max(20, "máximo 20 caracteres"),
-
-      secondLastName: Yup.string()
-        .matches(/^[A-Za-z]+$/, "El Segundo Apellido solo debe contener letras")
-        .max(20, "máximo 20 caracteres"
-        ),
-
-      firstNameDepr: Yup.string()
-        .matches(/^[A-Za-z]+$/, "El nombre solo debe contener letras")
-        .required("Primer nombre requerido")
-        .max(20, "máximo 20 caracteres"),
-
-      middleNameDepr: Yup.string()
-        .matches(/^[A-Za-z]+$/, "El Segundo Nombre solo debe contener letras")
-        .max(20, "máximo 20 caracteres"),
-
-      lastNameDepr: Yup.string()
-        .matches(/^[A-Za-z]+$/, "El Apellido solo debe contener letras")
-        .required("Apellido requerido")
-        .max(20, "máximo 20 caracteres"),
-
-      secondLastNameDepr: Yup.string()
-        .matches(/^[A-Za-z]+$/, "El Segundo Apellido solo debe contener letras")
-        .max(20, "máximo 20 caracteres"
-        ),
-
-      email: Yup.string()
-        .email("Dirección de correo electrónico no válida")
-        .required("Correo electronico es requerido")
-        .max(100, "máximo 100 caracteres"),
-
-      phoneNumber: Yup.string()
-        .required("Teléfono requerido")
-        .matches(/^[0-9]+$/, "solo debe contener números")
-        .max(20, "máximo 20 caracteres"),
-
-      socialSecurity: Yup.string()
-        .required("Seguro Social requerido")
-        .matches(/^[0-9]+$/, "solo debe contener números")
-        .max(20, "máximo 20 caracteres"),
-
-      birthdate: Yup.string().required("Fecha de nacimiento requerida"),
-
-      password: Yup.string()
-        .required("Contraseña no puede estar vacío")
-        .matches(
-          /^(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\;])[a-zA-Z\d!@#$%^&*()\;]{8,}$/,
-          "La contraseña debe tener al menos 8 caracteres, un número, una letra mayúscula y un símbolo"
-        )
-        .max(20, "máximo 20 caracteres"),
-      repeatPassword: Yup.string()
-        .required("Confirmar Contraseña no puede estar vacío")
-        .oneOf([Yup.ref('password')], 'Las contraseñas deben coincidir')
-        .max(20, "máximo 20 caracteres"),
-    }),
-
-    onSubmit: () => {
-      setValidate(true);
+    validationSchema: registerValidation, // Aquí asegúrate de pasar el objeto globalValidations
+    onSubmit: async () => {
+      await sendUserForRegister();
     },
-
-    validateOnChange: true, // Esto garantiza que se validen los campos con cada cambio
+    validateOnChange: true,
     validateOnBlur: true,
   });
-
 
   const navigate = useNavigate();
 
